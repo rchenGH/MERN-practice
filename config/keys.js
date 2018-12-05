5695c9aab5720a52ec650fae19bb73af65b087be
Raymond Chen <raymondchen86@hotmail.com>
2018-12-04 17:36:49 -0800

    add jwt key to get token once send response

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://CanadianRay:trustno1mlab@ds123454.mlab.com:23454/devconnector-rchen'
-}
+  mongoURI: 'mongodb://CanadianRay:trustno1mlab@ds123454.mlab.com:23454/devconnector-rchen',
+  secretOrKey: 'secret'
+};
