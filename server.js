5eb7b16ca1cbdbc82f51a31c53e7f47f004eeea8
Raymond Chen <raymondchen86@hotmail.com>
2018-12-06 17:39:44 -0800

    add passport jwt authentication stategy

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
-
 const profile = require('./routes/api/profile');
-
 const posts = require('./routes/api/posts');
 
 const app = express();
@@ -23,7 +22,11 @@ mongoose
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World'));
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require('./config/passport')(passport)
 
 // Use Routes
 app.use('/api/users', users);
