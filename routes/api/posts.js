a1f902014edb3b149ed287cd951cc8147aaa2c57
Raymond Chen <raymondchen86@hotmail.com>
2019-01-06 18:07:25 -0800

    add like and unlike route to post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -74,9 +74,58 @@ router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, re
                     if(post.user.toString() !== req.user.id) {
                         return res.status(401).json({ notauthorized: 'User not authorized' })
                     }
-
                     // Delete
                     post.remove().then( () => res.json({ success: true }) )
+                })
+                .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+        })
+});
+
+// @route   POST api/posts/like/:id
+// @desc    Like post
+// @access  Private
+router.post('/like/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            Post.findById(req.params.id)
+                .then(post => {
+                    // check to see if the user has liked already
+                    if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0){
+                        return res.status(400).json({ alreadyliked: 'User already liked this post'});
+                    }
+
+                    // Add user id to likes array
+                    post.likes.unshift({ user: req.user.id });
+
+                    post.save().then(post => res.json(post))
+                })
+                .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+        })
+});
+
+// @route   DELETE api/posts/unlike/:id
+// @desc    Unlike post
+// @access  Private
+router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            Post.findById(req.params.id)
+                .then(post => {
+                    // check to see if the user has liked already
+                    if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0){
+                        return res.status(400).json({ alreadyliked: 'You have not yet liked this post'});
+                    }
+
+                    // Get remove index
+                    const removeIndex = post.likes
+                        .map(item => item.user.toString())
+                        .indexOf(req.user.id);
+
+                    // Splice out of array
+                    post.likes.splice(removeIndex, 1);
+
+                    // Save
+                    post.save().then(post => res.json(post));
                 })
                 .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
         })
