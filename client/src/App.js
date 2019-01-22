484a9666a57ef4540cbdbcf4132723b07854c8c5
Raymond Chen <raymondchen86@hotmail.com>
2019-01-21 20:37:11 -0800

    logout should redirect user to login page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser } from './actions/authActions';
+import { logoutUser } from './actions/authActions';
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -25,6 +26,15 @@ if(localStorage.jwtToken){
   // Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
 
+  // check for expired token
+  const currentTime = Date.now() / 1000;
+  if(decoded.exp < currentTime){
+    // Logout user
+    store.dispatch(logoutUser());
+    // TODO: clear current profile
+    // Redirect to login
+    window.location.href = '/login';
+  }
 }
 
 class App extends Component {
