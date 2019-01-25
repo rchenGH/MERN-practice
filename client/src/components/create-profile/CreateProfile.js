dd8bacd90735b726600a6f900d9e3c9381125137
Raymond Chen <raymondchen86@hotmail.com>
2019-01-24 19:28:10 -0800

    add dropdown menu to CreateProfile component

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -38,8 +38,23 @@ class CreateProfile extends Component {
         this.setState({ [e.target.name]: e.target.value });
     }
 
+
+
     render() {
         const {errors} = this.state;
+
+        // Select options for status
+        const options = [
+            { label: '* Select Professional Status', value: 0 },
+            { label: 'Junior Developer', value: 'Junior Developer' },
+            { label: 'Intermediate Developer', value: 'Intermediate Developer' },
+            { label: 'Senior Developer', value: 'Senior Developer' },
+            { label: 'Manager', value: 'Manager' },
+            { label: 'Student or Learning', value: 'Student or Learning' },
+            { label: 'Instructor or Teacher', value: 'Instructor or Teacher' },
+            { label: 'Intern', value: 'Intern' },
+            { label: 'Other', value: 'Other' },
+        ];
 
         return (
             <div className="create-profile">
@@ -60,6 +75,15 @@ class CreateProfile extends Component {
                                     errors={errors.handle}
                                     info="A unique handle for your profile URL. Your full name, company name, nickname."
                                 />
+                                <SelectListGroup 
+                                    placeholder="Status"
+                                    name="status"
+                                    value={this.state.status}
+                                    onChange={this.onChange}
+                                    options={options}
+                                    errors={errors.status}
+                                    info="Give us an idea of where you are at in your career."
+                                />                    
                             </form>
                         </div>
                     </div>
