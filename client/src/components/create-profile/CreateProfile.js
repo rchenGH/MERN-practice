d0ac82bac45926914793823aa12c08c08eb034fc
Raymond Chen <raymondchen86@hotmail.com>
2019-01-25 05:00:34 -0800

    fix social network link button to prevent submission

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -203,7 +203,9 @@ class CreateProfile extends Component {
                                     info="Tell us a little about yourself"
                                 />
                                 <div className="mb-3">
-                                    <button onClick={() => {
+                                    <button 
+                                        type="button"
+                                        onClick={() => {
                                         this.setState(prevState => ({
                                             // will toggle the state, displaySocialInputs
                                             displaySocialInputs: !prevState.displaySocialInputs
