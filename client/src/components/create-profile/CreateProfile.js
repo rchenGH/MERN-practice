09bd41980840f5f050b9615e4096a82c2fe82276
Raymond Chen <raymondchen86@hotmail.com>
2019-01-25 04:52:56 -0800

    add action for createProfile, and add profile state to submit in createProfile component

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import TextFieldGroup from '../common/TextFieldGroup';
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
 import InputGroup from '../common/InputGroup';
 import SelectListGroup from '../common/SelectListGroup';
+import {createProfile} from '../../actions/profileActions';
 
 class CreateProfile extends Component {
 
@@ -29,16 +31,35 @@ class CreateProfile extends Component {
        };
     }
 
-    onSubmit(e) {
+    componentWillReceiveProps(nextProps){
+        if(nextProps.errors){
+            this.setState({errors: nextProps.errors})
+        }
+    }
+
+    onSubmit =(e) => {
         e.preventDefault();
-        console.log('submit');
+        const profileData = {
+            handle: this.state.handle,
+            company: this.state.company,
+            website: this.state.website,
+            location: this.state.location,
+            status: this.state.status,
+            skills: this.state.skills,
+            githubusername: this.state.githubusername,
+            bio: this.state.bio,
+            twitter: this.state.twitter,
+            facebook: this.state.facebook,
+            linkedin: this.state.linkedin,
+            youtube: this.state.youtube,
+            instagram: this.state.instagram,
+        }
+        this.props.createProfile(profileData, this.props.history);
     }
 
     onChange = (e) => {
         this.setState({ [e.target.name]: e.target.value });
     }
-
-
 
     render() {
         const {errors, displaySocialInputs} = this.state;
@@ -190,7 +211,7 @@ class CreateProfile extends Component {
                                     }} className="btn btn-light">
                                         Add Social Network Link
                                     </button>
-                                    <span className="text-muted">Optional</span>
+                                    <span className="text-muted"> Optional</span>
                                 </div>
                                 { socialInputs }
                                 <input type="submit" value="Submit" className="btn btn-info btn-block mt-4" />
@@ -213,4 +234,4 @@ const mapStateToProps = state => ({
     errors: state.errors
 })
 
-export default connect(mapStateToProps)(CreateProfile);
+export default connect(mapStateToProps, { createProfile })(withRouter(CreateProfile));
