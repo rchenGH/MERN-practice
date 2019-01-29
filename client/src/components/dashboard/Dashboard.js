717804266224228c69bb742d4e944cd17ae2a056
Raymond Chen <raymondchen86@hotmail.com>
2019-01-28 18:55:35 -0800

    add delete account action and delete function / button to dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 import { connect } from 'react-redux';
-import { getCurrentProfile } from '../../actions/profileActions';
+import { getCurrentProfile, deleteAccount } from '../../actions/profileActions';
 import Spinner from '../common/Spinner';
 import ProfileActions from './ProfileActions';
 
@@ -10,41 +10,49 @@ class Dashboard extends Component {
     componentDidMount(){
         this.props.getCurrentProfile();
     }
-  render() {
-    const { user } = this.props.auth;
-    const { profile, loading } = this.props.profile;
 
-    let dashboardContent;
-
-    if(profile === null || loading ){
-      dashboardContent = <Spinner />
-    } else {
-      // Check if logged in user has profile data
-      if(Object.keys(profile).length > 0){
-        dashboardContent = (
-          <div>
-            <p className="lead text-muted">
-              Welcome 
-              <Link to={`/profile/${profile.handle}`}>
-              {" " + user.name }
-              </Link>
-            </p>
-            <ProfileActions />
-          </div>
-        )
-      } else {
-        // User is logged in but has no profile
-        dashboardContent = (
-          <div>
-            <p className="lead text-muted">Welcome { user.name }</p>
-            <p>You have not yet setup a profile, please add some info</p>
-            <Link to="/create-profile" className="btn btn-lg btn-info">
-              Create Profile
-            </Link>
-          </div>
-        )
-      }
+    onDeleteClick = (e) =>{
+      this.props.deleteAccount();
     }
+
+    render() {
+      const { user } = this.props.auth;
+      const { profile, loading } = this.props.profile;
+
+      let dashboardContent;
+
+      if(profile === null || loading ){
+        dashboardContent = <Spinner />
+      } else {
+        // Check if logged in user has profile data
+        if(Object.keys(profile).length > 0){
+          dashboardContent = (
+            <div>
+              <p className="lead text-muted">
+                Welcome 
+                <Link to={`/profile/${profile.handle}`}>
+                {" " + user.name }
+                </Link>
+              </p>
+              <ProfileActions />
+              {/* TODO: exp and edu */}
+              <div style={{ marginBottom: '60px'}} />
+              <button onClick={this.onDeleteClick} className="btn btn-danger">Delete My Account</button>
+            </div>
+          )
+        } else {
+          // User is logged in but has no profile
+          dashboardContent = (
+            <div>
+              <p className="lead text-muted">Welcome { user.name }</p>
+              <p>You have not yet setup a profile, please add some info</p>
+              <Link to="/create-profile" className="btn btn-lg btn-info">
+                Create Profile
+              </Link>
+            </div>
+          )
+        }
+      }
 
     return (
       <div className="dashboard">
@@ -65,8 +73,9 @@ class Dashboard extends Component {
 
 Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
+  deleteAccount: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
-  profile: PropTypes.object.isRequired
+  profile: PropTypes.object.isRequired,
 }
 
 const mapStateToProps = state => ({
@@ -74,4 +83,4 @@ const mapStateToProps = state => ({
   auth: state.auth
 })
 
-export default connect(mapStateToProps, {getCurrentProfile})(Dashboard);
+export default connect(mapStateToProps, {getCurrentProfile, deleteAccount})(Dashboard);
