45d91d16bd4059ee1e8912153aa4064a784594e8
Raymond Chen <raymondchen86@hotmail.com>
2019-01-30 00:43:59 -0800

    add error setState in will recieve props, data to props, and action to experience

--- client/src/components/add-credentials/AddExperience.js
+++ client/src/components/add-credentials/AddExperience.js
@@ -4,7 +4,7 @@ import TextFieldGroup from '../common/TextFieldGroup';
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-
+import { addExperience } from '../../actions/profileActions';
  
 class AddExperience extends Component {
 
@@ -34,9 +34,24 @@ class AddExperience extends Component {
         });
     }
 
+    componentWillReceiveProps(nextProps){
+        if(nextProps.errors){
+            this.setState({errors: nextProps.errors})
+        }
+    }
+
     onSubmit = (e) => {
         e.preventDefault();
-        console.log('submit')
+        const expData = {
+            company: this.state.company,
+            title: this.state.title,
+            location: this.state.location,
+            from: this.state.from,
+            to: this.state.to,
+            current: this.state.current,
+            description: this.state.description
+        }
+        this.props.addExperience(expData, this.props.history)
     }
 
     render() {
@@ -107,7 +122,7 @@ class AddExperience extends Component {
                                     />
                                     <label htmlFor="current" className="form-check=label">Current Job</label>
                                 </div>
-                                <TextFieldGroup 
+                                <TextAreaFieldGroup 
                                     placeholder="Job Description"
                                     name="description"
                                     value={this.state.description}
@@ -130,6 +145,7 @@ class AddExperience extends Component {
 }
 
 AddExperience.propTypes = ({
+    addExperience: PropTypes.func.isRequired,
     profile: PropTypes.object.isRequired,
     errors: PropTypes.object.isRequired
 });
@@ -140,4 +156,4 @@ const mapStateToProps = state => ({
 });
 
 // redirect, wrap with 'withRouter'
-export default connect(mapStateToProps)(withRouter(AddExperience));
+export default connect(mapStateToProps, { addExperience })(withRouter(AddExperience));
