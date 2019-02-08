b4c39f08961d8cfceccbd11755cef6511ee0b7c1
Raymond Chen <raymondchen86@hotmail.com>
2019-02-07 17:24:20 -0800

    add profile credentials component, map experience and education in component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -36,7 +36,7 @@ class Profile extends Component {
                 </div>
                 <ProfileHeader profile={profile} />
                 <ProfileAbout profile={profile}/>
-                <ProfileCreds />
+                <ProfileCreds education={profile.education} experience={profile.experience}/>
                 <ProfileGithub />
               </div>
 
