cdb2414c62910884f844987bdd5906f86c19c87c
Raymond Chen <raymondchen86@hotmail.com>
2019-02-10 02:42:23 -0800

    add posts route, post nav link, post form component, posts component, post reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import errorReducer from './errorReducer';
+import profileReducer from './profileReducer'
 import postReducer from './postReducer'
 
 export default combineReducers({
