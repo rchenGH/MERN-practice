6b215131e8ef999394262b05f585cd4ca285c468
Raymond Chen <raymondchen86@hotmail.com>
2019-02-13 17:37:25 -0800

    add addPost action to submit handle, state from reducer, and errors in componentWillRecieveProps

--- client/src/components/posts/PostForm.js
+++ client/src/components/posts/PostForm.js
@@ -14,9 +14,23 @@ class PostForm extends Component {
         }
     }
 
+    componentWillReceiveProps(newProps){
+        if(newProps.errors){
+            this.setState({errors: newProps.errors})
+        }
+    }
+
     onSubmit = (e) => {
         e.preventDefault();
-        console.log('submit')
+        const { user } = this.props.auth;
+        const newPost = {
+            text: this.state.text,
+            name: user.name,
+            avatar: user.avatar
+        }
+
+        this.props.addPost(newPost);
+        this.setState({text: ''})
     }
 
     onChange = (e) => {
@@ -52,4 +66,15 @@ class PostForm extends Component {
     }
 }
 
-export default PostForm;
+PostForm.propTypes = {
+    addPOst: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+    auth: state.auth,
+    errors: state.errors
+});
+
+export default connect(mapStateToProps, { addPost })(PostForm);
