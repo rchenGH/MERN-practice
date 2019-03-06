60f70b5b70b24c11c97c045192bd84a069ba5eab
Raymond Chen <raymondchen86@hotmail.com>
2019-03-06 00:04:19 -0800

    add prod and dev keys for security

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://CanadianRay:trustno1mlab@ds123454.mlab.com:23454/devconnector-rchen',
-  secretOrKey: 'secret'
-};
+if(process.env.NODE_ENV === 'production'){
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
