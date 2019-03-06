019876f59c855aceb4c158b4eba9a5c536339571
Raymond Chen <raymondchen86@hotmail.com>
2019-03-05 23:53:24 -0800

    add clear errors

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import {
     ADD_POST,
     GET_ERRORS,
+    CLEAR_ERRORS,
     GET_POSTS,
     GET_POST,
     POST_LOADING,
@@ -11,6 +12,7 @@ import {
 
 // Add Post
 export const addPost = postData => dispatch => {
+    dispatch(clearErrors());
     axios
         .post('/api/posts', postData)
         .then(res => 
@@ -118,6 +120,7 @@ export const removeLike = id => dispatch => {
 
 // Add Comment
 export const addComment = (postId, commentData) => dispatch => {
+    dispatch(clearErrors());
     axios
         .post(`/api/posts/comment/${postId}`, commentData)
         .then(res => 
@@ -156,5 +159,12 @@ export const deleteComment = (postId, commentId) => dispatch => {
 export const setPostLoading = () => {
     return {
         type: POST_LOADING
+    }
+}
+
+// Clear errors
+export const clearErrors = () => {
+    return {
+        type: CLEAR_ERRORS
     }
 }
